e1ba6c2e47f4c57a778fd7bb93bc567c739eb365
Alejandro <alejodev2022@gmail.com>
2025-11-11 01:51:53 -0500

    fix: corregir las importaciones de los componentes SEO y CardContacto en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import SEO from "./components/SEO.jsx";
+import SEO from "./components/SEO";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Tecnologias from "./components/Tecnologias/index.jsx";
 import Proyectos from "./components/Proyectos";
-import CardContacto from "./components/Contacto.jsx";
+import CardContacto from "./components/Contacto";
 import Footer from "./components/Footer";
 import AboutMeDetailed from "./pages/AboutMeDetailed";
 import KonamiCode from "./Game/KonnamiGame";
